603c0a58be9010cd052b8eb88d8b553110a8258a
Josef Matondang <josstemat@gmail.com>
2019-07-02 14:28:54 +0700

    Add getDeviceInfo.py

---
+++ getDeviceInfo.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import serial
+import time
+
+sensor = serial.Serial("/dev/ttyS0", baudrate = 9600, timeout = 0.5)
+sensor.flushInput()
+
+time.sleep(1)
+
+sensor.flushInput()
+sensor.write("\xFE\x2B\x0E\x04\x00\xA6\xF3")
+time.sleep(.5)
+
+response = sensor.read()
+print(response)
